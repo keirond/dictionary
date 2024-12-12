01422bb56fd58a03526b84ce6f3d2ebe83cad285
Bao. Dang <dang.hbaokody@gmail.com>
2024-12-12 18:40:51 +0000

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,74 @@
-import eslint from '@eslint/js';
-import eslintPluginReact from 'eslint-plugin-react';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import eslintPluginImport from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
+
+import eslint from '@eslint/js';
+
+import reactPlugin from 'eslint-plugin-react';
+
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierPluginRecommended from 'eslint-plugin-prettier/recommended';
 import prettierConfig from './prettier.config.js';
 
-const ignores = ['*.js', '*.mjs', 'dist/', 'node_modules/'];
-const jsRules = {};
+// import eslintPluginImport from 'eslint-plugin-import';
 
-export default tseslint.config({
-  ignores,
-  languageOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-    parser: tseslint.parser,
-    parserOptions: {
-      project: './tsconfig.json',
-      tsconfigRootDir: __dirname,
-    },
-  },
-  plugins: {
-    '@typescript-eslint': tseslint.plugin,
-    react: eslintPluginReact,
-    prettier: eslintPluginPrettier,
-    import: eslintPluginImport,
-  },
-  extends: [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintPluginPrettierRecommended,
-    eslintPluginImport.configs.recommended,
-  ],
-  rules: {
-    ...jsRules,
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        argsIgnorePattern: '^_',
+export default tseslint.config(
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
       },
-    ],
-    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
-    'prettier/prettier': ['warn', prettierConfig],
-  },
-  settings: {
-    react: {
-      version: 'detect',
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    extends: [...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
-});
+  {
+    files: ['**/*.jsx', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    extends: [prettierPluginRecommended],
+    rules: {
+      'prettier/prettier': ['warn', prettierConfig],
+    },
+  },
+  {
+    ignores: ['**/*.js', '**/*.mjs', 'dist/', 'node_modules/'],
+  }
+);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+const name: string = null;
 const sampleObject = {
     name: 'John Doe',
     age: 30,
@@ -13,7 +14,7 @@ function greetUser(user: { name: string; age: number }) {
     console.log(`Hello, ${user.name}! You are ${user.age} years old.`);
 }
 
-async function fetchData(url: string): Promise<{ data: string; status: number }> {
+async function fetchData(_url: string): Promise<{ data: string; status: number }> {
     return await new Promise((resolve) => {
         setTimeout(() => resolve({ data: 'Sample Data', status: 200 }), 1000);
     });
